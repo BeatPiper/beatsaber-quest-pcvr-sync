dd9ed12b70f1604b46331305a08a154948756800
D3SOX <nico@d3sox.me>
2022-11-23 22:07:32 +0100

    refactor: reuse relative path for game path

--- src/utils/pcPaths.ts
+++ src/utils/pcPaths.ts
@@ -13,6 +13,7 @@ export function getBeatSaberConfigPath(): string {
   const { sep } = path;
   const relativePath = `AppData${sep}LocalLow${sep}Hyperbolic Magnetism${sep}Beat Saber`;
   const platform = os.platform();
+
   if (platform === 'win32') {
     return `${process.env.USERPROFILE}${sep}${relativePath}`;
   } else if (platform === 'linux') {
@@ -30,15 +31,16 @@ export function getBeatSaberGamePath(): string {
   if (customGamePath) {
     return customGamePath;
   }
+
   const { sep } = path;
+  const relativePath = `steamapps${sep}common${sep}Beat Saber`;
   const platform = os.platform();
 
   if (platform === 'win32') {
-    return `${process.env['PROGRAMFILES(X86)']}${sep}Steam${sep}steamapps${sep}common${sep}Beat Saber`;
+    return `${process.env['PROGRAMFILES(X86)']}${sep}Steam${sep}${relativePath}`;
   } else if (platform === 'linux') {
-    return `${process.env.HOME}${sep}.local${sep}share${sep}Steam/steamapps${sep}common${sep}Beat Saber`;
+    return `${process.env.HOME}${sep}.local${sep}share${sep}Steam/${relativePath}`;
   }
-
   throw new Error('Unsupported platform');
 }
 
